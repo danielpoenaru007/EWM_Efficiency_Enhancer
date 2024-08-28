cf99d348f148255216b37da4df3c90c5b39f7ea2
Daniel Poenaru <daniel.poenaru@bearingpoint.com>
2024-08-28 09:28:13 +0000

    Scroll on table automatically

--- webapp/controller/Materials.controller.js
+++ webapp/controller/Materials.controller.js
@@ -1,55 +1,58 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("bearingpoint.ewm.materialmaintenance.controller.Materials", {
-        onInit: function () {
+        return Controller.extend("bearingpoint.ewm.materialmaintenance.controller.Materials", {
+            onInit: function () {
+                var oSmartTable = this.getView().byId("LineItemsSmartTable");
+                var oTable = oSmartTable.getTable();
+                oTable.setGrowingThreshold(20);
+                oTable.setGrowingScrollToLoad(true);
+            },
 
-        },
+            onEditToggled: function (oEvent) {
+                var oSmartTable = oEvent.getSource();
+                var oSmartTableToolbarContent = oSmartTable.getToolbar().getContent();
 
-        onEditToggled: function (oEvent) {
-            var oSmartTable = oEvent.getSource();
-            var oSmartTableToolbarContent = oSmartTable.getToolbar().getContent();
+                function isEditButton(element) {
+                    try {
+                        if (element.getProperty("accesskey") == "d")
+                            return element;
+                    } catch (error) {
 
-            function isEditButton (element){
-                try {
-                    if ( element.getProperty("accesskey") == "d" )
-                        return element;
-                } catch (error) {
-                    
+                    }
                 }
+                var oEditToggleBtn = oSmartTableToolbarContent.find(isEditButton);
+
+                // var oEditToggleBtn = sap.ui.getCore().byId("container-smarttable---ViewSmartTable--LineItemsSmartTable-btnEditToggle");
+
+                if (oSmartTable.getEditable()) {
+                    // Toggle to Save Mode
+                    oEditToggleBtn.setIcon("sap-icon://save");
+
+                } else {
+                    // Toggle to Edit Mode
+                    oEditToggleBtn.setIcon("sap-icon://edit");
+
+                    // Save Data to database
+                    this.onSaveData();
+                }
+            },
+
+            onSaveData: function () {
+                var oModel = this.getView().getModel("yourModel");
+
+                // Trigger the submitChanges method to save the edits
+                // oModel.submitChanges({
+                //     success: function () {
+                //         MessageToast.show("Changes saved successfully.");
+                //     },
+                //     error: function () {
+                //         MessageToast.show("Failed to save changes.");
+                //     }
+                // });
             }
-            var oEditToggleBtn = oSmartTableToolbarContent.find(isEditButton);
-
-           // var oEditToggleBtn = sap.ui.getCore().byId("container-smarttable---ViewSmartTable--LineItemsSmartTable-btnEditToggle");
-
-            if (oSmartTable.getEditable()) {
-                // Toggle to Save Mode
-                oEditToggleBtn.setIcon("sap-icon://save");
-
-            } else {
-                // Toggle to Edit Mode
-                oEditToggleBtn.setIcon("sap-icon://edit");
-
-                // Save Data to database
-               this.onSaveData();
-            }
-        },
-
-        onSaveData: function () {
-            var oModel = this.getView().getModel("yourModel");
-
-            // Trigger the submitChanges method to save the edits
-            // oModel.submitChanges({
-            //     success: function () {
-            //         MessageToast.show("Changes saved successfully.");
-            //     },
-            //     error: function () {
-            //         MessageToast.show("Failed to save changes.");
-            //     }
-            // });
-        }
+        });
     });
-});
